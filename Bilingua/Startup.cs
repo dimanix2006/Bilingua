ca2f4a3054dd6ef29230ab859550e59e47a1682b
dimanix2006 <dimatver@bk.ru>
2018-04-29 22:04:06 +0300

    Создание проекта

---
+++ Bilingua/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bilingua.Startup))]
+namespace Bilingua
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
